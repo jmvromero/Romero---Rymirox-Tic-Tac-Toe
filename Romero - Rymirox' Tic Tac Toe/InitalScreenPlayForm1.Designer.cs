de9244229943eac61748fee497bc666bc40eb28a
rymirox <jm.romero011@yahoo.com>
2022-06-03 15:28:42 +0800

    Button 1 has a complete function and I will put this function to other buttons

--- Romero - Rymirox' Tic Tac Toe/InitalScreenPlayForm1.Designer.cs
+++ Romero - Rymirox' Tic Tac Toe/InitalScreenPlayForm1.Designer.cs
@@ -47,46 +47,50 @@
             this.txtbx7 = new System.Windows.Forms.TextBox();
             this.txtbx8 = new System.Windows.Forms.TextBox();
             this.txtbx9 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // B1
             // 
             this.B1.BackColor = System.Drawing.Color.Pink;
             this.B1.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B1.Location = new System.Drawing.Point(116, 132);
+            this.B1.Location = new System.Drawing.Point(116, 117);
             this.B1.Name = "B1";
             this.B1.Size = new System.Drawing.Size(140, 144);
             this.B1.TabIndex = 0;
             this.B1.Text = "X / O";
             this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.B1_Click);
             // 
             // B2
             // 
             this.B2.BackColor = System.Drawing.Color.Pink;
             this.B2.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B2.Location = new System.Drawing.Point(292, 132);
+            this.B2.Location = new System.Drawing.Point(292, 117);
             this.B2.Name = "B2";
             this.B2.Size = new System.Drawing.Size(140, 144);
             this.B2.TabIndex = 1;
             this.B2.Text = "X / O";
             this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.B2_Click);
             // 
             // B3
             // 
             this.B3.BackColor = System.Drawing.Color.Pink;
             this.B3.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B3.Location = new System.Drawing.Point(464, 132);
+            this.B3.Location = new System.Drawing.Point(464, 117);
             this.B3.Name = "B3";
             this.B3.Size = new System.Drawing.Size(140, 144);
             this.B3.TabIndex = 3;
             this.B3.Text = "X / O";
             this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.B3_Click);
             // 
             // B4
             // 
             this.B4.BackColor = System.Drawing.Color.Pink;
             this.B4.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B4.Location = new System.Drawing.Point(116, 326);
+            this.B4.Location = new System.Drawing.Point(116, 311);
             this.B4.Name = "B4";
             this.B4.Size = new System.Drawing.Size(140, 144);
             this.B4.TabIndex = 2;
@@ -97,7 +101,7 @@
             // 
             this.B5.BackColor = System.Drawing.Color.Pink;
             this.B5.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B5.Location = new System.Drawing.Point(292, 326);
+            this.B5.Location = new System.Drawing.Point(292, 311);
             this.B5.Name = "B5";
             this.B5.Size = new System.Drawing.Size(140, 144);
             this.B5.TabIndex = 5;
@@ -108,7 +112,7 @@
             // 
             this.B6.BackColor = System.Drawing.Color.Pink;
             this.B6.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B6.Location = new System.Drawing.Point(464, 326);
+            this.B6.Location = new System.Drawing.Point(464, 311);
             this.B6.Name = "B6";
             this.B6.Size = new System.Drawing.Size(140, 144);
             this.B6.TabIndex = 4;
@@ -119,7 +123,7 @@
             // 
             this.B7.BackColor = System.Drawing.Color.Pink;
             this.B7.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B7.Location = new System.Drawing.Point(116, 521);
+            this.B7.Location = new System.Drawing.Point(116, 506);
             this.B7.Name = "B7";
             this.B7.Size = new System.Drawing.Size(140, 144);
             this.B7.TabIndex = 8;
@@ -130,7 +134,7 @@
             // 
             this.B8.BackColor = System.Drawing.Color.Pink;
             this.B8.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B8.Location = new System.Drawing.Point(292, 521);
+            this.B8.Location = new System.Drawing.Point(292, 506);
             this.B8.Name = "B8";
             this.B8.Size = new System.Drawing.Size(140, 144);
             this.B8.TabIndex = 7;
@@ -141,7 +145,7 @@
             // 
             this.B9.BackColor = System.Drawing.Color.Pink;
             this.B9.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.B9.Location = new System.Drawing.Point(464, 521);
+            this.B9.Location = new System.Drawing.Point(464, 506);
             this.B9.Name = "B9";
             this.B9.Size = new System.Drawing.Size(140, 144);
             this.B9.TabIndex = 6;
@@ -153,7 +157,7 @@
             this.txtbx1.BackColor = System.Drawing.Color.Pink;
             this.txtbx1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.txtbx1.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx1.Location = new System.Drawing.Point(116, 99);
+            this.txtbx1.Location = new System.Drawing.Point(116, 84);
             this.txtbx1.Name = "txtbx1";
             this.txtbx1.Size = new System.Drawing.Size(140, 27);
             this.txtbx1.TabIndex = 9;
@@ -163,7 +167,7 @@
             // 
             this.txtbx2.BackColor = System.Drawing.Color.Pink;
             this.txtbx2.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx2.Location = new System.Drawing.Point(292, 99);
+            this.txtbx2.Location = new System.Drawing.Point(292, 84);
             this.txtbx2.Name = "txtbx2";
             this.txtbx2.Size = new System.Drawing.Size(140, 27);
             this.txtbx2.TabIndex = 10;
@@ -173,7 +177,7 @@
             // 
             this.txtbx3.BackColor = System.Drawing.Color.Pink;
             this.txtbx3.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx3.Location = new System.Drawing.Point(464, 99);
+            this.txtbx3.Location = new System.Drawing.Point(464, 84);
             this.txtbx3.Name = "txtbx3";
             this.txtbx3.Size = new System.Drawing.Size(140, 27);
             this.txtbx3.TabIndex = 11;
@@ -183,7 +187,7 @@
             // 
             this.txtbx4.BackColor = System.Drawing.Color.Pink;
             this.txtbx4.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx4.Location = new System.Drawing.Point(116, 293);
+            this.txtbx4.Location = new System.Drawing.Point(116, 278);
             this.txtbx4.Name = "txtbx4";
             this.txtbx4.Size = new System.Drawing.Size(140, 27);
             this.txtbx4.TabIndex = 12;
@@ -193,7 +197,7 @@
             // 
             this.txtbx5.BackColor = System.Drawing.Color.Pink;
             this.txtbx5.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx5.Location = new System.Drawing.Point(292, 293);
+            this.txtbx5.Location = new System.Drawing.Point(292, 278);
             this.txtbx5.Name = "txtbx5";
             this.txtbx5.Size = new System.Drawing.Size(140, 27);
             this.txtbx5.TabIndex = 13;
@@ -203,7 +207,7 @@
             // 
             this.txtbx6.BackColor = System.Drawing.Color.Pink;
             this.txtbx6.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx6.Location = new System.Drawing.Point(464, 293);
+            this.txtbx6.Location = new System.Drawing.Point(464, 278);
             this.txtbx6.Name = "txtbx6";
             this.txtbx6.Size = new System.Drawing.Size(140, 27);
             this.txtbx6.TabIndex = 14;
@@ -213,7 +217,7 @@
             // 
             this.txtbx7.BackColor = System.Drawing.Color.Pink;
             this.txtbx7.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx7.Location = new System.Drawing.Point(116, 488);
+            this.txtbx7.Location = new System.Drawing.Point(116, 473);
             this.txtbx7.Name = "txtbx7";
             this.txtbx7.Size = new System.Drawing.Size(140, 27);
             this.txtbx7.TabIndex = 15;
@@ -223,7 +227,7 @@
             // 
             this.txtbx8.BackColor = System.Drawing.Color.Pink;
             this.txtbx8.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx8.Location = new System.Drawing.Point(292, 488);
+            this.txtbx8.Location = new System.Drawing.Point(292, 473);
             this.txtbx8.Name = "txtbx8";
             this.txtbx8.Size = new System.Drawing.Size(140, 27);
             this.txtbx8.TabIndex = 16;
@@ -233,11 +237,23 @@
             // 
             this.txtbx9.BackColor = System.Drawing.Color.Pink;
             this.txtbx9.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.txtbx9.Location = new System.Drawing.Point(464, 488);
+            this.txtbx9.Location = new System.Drawing.Point(464, 473);
             this.txtbx9.Name = "txtbx9";
             this.txtbx9.Size = new System.Drawing.Size(140, 27);
             this.txtbx9.TabIndex = 17;
             this.txtbx9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button1.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(292, 692);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 34);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Results";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // InitalScreenPlayForm1
             // 
@@ -245,6 +261,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Romero___Rymirox__Tic_Tac_Toe.Properties.Resources._1ccbae11952e748c403b5d98cdb07fab;
             this.ClientSize = new System.Drawing.Size(727, 763);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.txtbx9);
             this.Controls.Add(this.txtbx8);
             this.Controls.Add(this.txtbx7);
@@ -294,5 +311,6 @@
         private TextBox txtbx7;
         private TextBox txtbx8;
         private TextBox txtbx9;
+        private Button button1;
     }
 }
--- Romero - Rymirox' Tic Tac Toe/InitalScreenPlayForm1.cs
+++ Romero - Rymirox' Tic Tac Toe/InitalScreenPlayForm1.cs
@@ -16,5 +16,41 @@ namespace Romero___Rymirox__Tic_Tac_Toe
         {
             InitializeComponent();
         }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            String input1 = txtbx1.Text;
+            MessageBox.Show(input1);
+
+            Boolean willExecute = input1 == "X" || input1 == "O";
+            if (willExecute)
+            {
+                //true
+                txtbx1.Enabled = false;
+                B1.Enabled = false;
+            }
+            else
+            {
+                //false
+                txtbx1.Enabled = true;
+                MessageBox.Show("Try Again");
+                B1.Enabled = true;
+            }
+        }
+
+
+        private void B2_Click(object sender, EventArgs e)
+        {
+            String input2 = txtbx2.Text;
+            MessageBox.Show(input2);
+            txtbx2.Enabled = false;
+        }
+
+        private void B3_Click(object sender, EventArgs e)
+        {
+            String input3 = txtbx2.Text;
+            MessageBox.Show(input3);
+            txtbx3.Enabled = false;
+        }
     }
 }
